d6e6923f0d1ac723288f6d7f342a0a5f1d8ddd54
EstebanCamejo <125935044+EstebanCamejo04@users.noreply.github.com>
2024-08-15 10:48:22 -0300

    Trigger GitHub Actions

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,8 +7,7 @@ import { useContextGlobal } from "../Context/global.context";
 const Card = ({ dentist }) => {
   const { name, username, id } = dentist;
   const { dispatch, favs } = useContextGlobal();
-  //hola
-  //nos vemo
+  //hello
 
   const findFav = favs.find((fav) => fav.id == dentist.id);
 
